a14e3407da1ce5693a260bd55f79608c1088188b
pisonpeakgroup <pisonpeakgroup@gmail.com>
2017-05-01 09:55:59 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,20 +1,26 @@
-//javascript addition operator
+//javascript  addition operator
 var a = 14;
 var b = 26;
-var c = a + b;
+var cal = a + b;
+alert (cal);
 
 
 //js subtration operator
 var a = 14;
 var b = 26;
-var c = a - b;
+var cal = a - b;
+alert(cal);
+
 
 //js multiplication operator
 var a = 14;
 var b = 26;
-var c = a * b;
+var cal = a * b;
+alert(cal);
+
 
 //js division operator
 var a = 14;
 var b = 26;
-var c = a / b;
+var cal = a / b;
+alert(cal);
