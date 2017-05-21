ae274cf198e8078d55424a5630fb6bf306b5ed60
pisonpeakgroup <pisonpeakgroup@gmail.com>
2017-05-21 13:57:38 +0100

    Update calculator-spec.js

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,31 +1,22 @@
-MathUtils = function() {};
+describe("MathUtils", function() {
+    var calc;
  
-MathUtils.prototype.sum = function(number1, number2) {
-        return number1 + number2;
-}
+    //This will be called before running each spec
+    beforeEach(function() {
+        calc = new MathUtils();
+    });
  
-MathUtils.prototype.substract = function(number1, number2) {
-    return number1 - number2;
-}
+    describe("when calc is used to peform basic math operations", function(){
+         
+        //Spec for sum operation
+        it("should be able to calculate sum of 3 and 5", function() {
+            expect(calc.sum(3,5)).toEqual(8);
+        });
  
-MathUtils.prototype.multiply = function(number1, number2) {
-    return number1 * number2;
-}
- 
-MathUtils.prototype.divide = function(number1, number2) {
-    return number1 / number2;
-}
- 
-MathUtils.prototype.average = function(number1, number2) {
-    return (number1 + number2) / 2;
-}
- 
-MathUtils.prototype.factorial = function(number) {
-    if (number < 0) {
-        throw new Error("There is no factorial for negative numbers");
-    } else if (number == 1 || number == 0) {
-        return 1;
-    } else {
-        return number * this.factorial(number - 1);
-    }
-}
+        //Spec for multiply operation
+        it("should be able to multiply 10 and 40", function() {
+            expect(calc.multiply(10, 40)).toEqual(400);
+               });
+         
+    });
+});
