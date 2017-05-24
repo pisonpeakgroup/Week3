6b07fa698a61a341ab262e66a85a5d9757b572a3
pisonpeakgroup <pisonpeakgroup@gmail.com>
2017-05-24 14:54:01 +0100

    Update calculator-spec.js

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -8,8 +8,8 @@ describe("MathUtils", function() {
  
     describe("when calc is used to peform basic math operations", function(){
          
-        //Spec for sum operation
-        it("should be able to calculate sum of 3 and 5", function() {
+        //Spec for sum operationjasmine
+            it("should be able to calculate sum of 3 and 5", function() {
             expect(calc.sum(3,5)).toEqual(8);
         });
  
@@ -17,6 +17,12 @@ describe("MathUtils", function() {
         it("should be able to multiply 10 and 40", function() {
             expect(calc.multiply(10, 40)).toEqual(400);
                });
-         
+        //Spec for division operation
+        it("should be able to calculate division of 10 and 5", function(){
+            expect(calc.divide(10, 5)).toEqual(2);             
+    });
+    //Spec for Addition operation
+    it("should be able to calculate addition of 10 and 5", function() {
+        expect(calc.add(10,5)).toEqual(15);
     });
 });
